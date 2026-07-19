23f8b8fe90b5f6fb6ccc1be11e98391641d6c028
marthdholariya <marthdholariya7@gmail.com>
2026-07-19 17:10:29 +0530

    Initial commit

---
+++ PowerWithoutMul.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PowerWithoutMul {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Base and Power: ");
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        function(n,m);
+
+        sc.close();
+    }
+
+    static void function(int n, int m){
+
+        for (int i=0; i<n; i++){
+            
+        }
+
+    }
+}
